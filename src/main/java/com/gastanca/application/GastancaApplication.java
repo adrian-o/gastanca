16a6cc8634d0f4badfb231892cee114ff610a0a2
Adriano Ohana <adriano.ohana@gmail.com>
2020-04-10 18:42:52 -0300

    Primeira versão[

---
+++ src/main/java/com/gastanca/application/GastancaApplication.java
@@ -0,0 +1,13 @@
+package com.gastanca.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gastanca/application/GastancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastanca.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
